f4540febbe139149739022443e591a6b56d633dd
x215617 <X215617@prservicos.com.br>
2020-06-28 10:17:56 -0300

    First commit - project creation

---
+++ src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgdi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//no need for code here
+	}
+
+}
