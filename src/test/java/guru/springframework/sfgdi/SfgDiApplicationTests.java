0d3f6729bad79aafb24178519d702612b4b63d8a
x215617 <X215617@prservicos.com.br>
2020-06-28 10:29:48 -0300

    Bug fix in the main testing class

--- src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
+++ src/test/java/guru/springframework/sfgdi/SfgDiApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SfgDiApplicationTests {
+public class SfgDiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 		//no need for code here
 	}
 
